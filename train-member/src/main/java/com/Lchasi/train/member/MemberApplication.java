96c63d40871fe87f53eb4b16e65d17fd42b07758
Lchasi <1569788669@qq.com>
2024-06-20 17:57:02 +0800

    修改启动日志，增加/member,修改logback日志

--- train-member/src/main/java/com/Lchasi/train/member/MemberApplication.java
+++ train-member/src/main/java/com/Lchasi/train/member/MemberApplication.java
@@ -1,13 +1,21 @@
 package com.Lchasi.train.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment enx = app.run(args).getEnvironment();//拿到系统环境
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttp://127.0.0.1:{}", enx.getProperty("server.port"));//获得系统端口
+//        SpringApplication.run(MemberApplication.class, args);
     }
 
 }
