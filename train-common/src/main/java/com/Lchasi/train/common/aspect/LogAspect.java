12cb47d7320bc2cbef3873d07e726f39be9b7d5a
Lchasi <1569788669@qq.com>
2024-06-21 11:47:55 +0800

    配置公共模块common，用于放公共的类，比如工具类，拦截器，AQP,常量，枚举，公共配置等

--- train-common/src/main/java/com/Lchasi/train/common/aspect/LogAspect.java
+++ train-common/src/main/java/com/Lchasi/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.Lchasi.train.member.aspect;
+package com.Lchasi.train.common.aspect;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
