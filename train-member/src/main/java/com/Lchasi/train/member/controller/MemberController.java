cbd8aa3e5760c1927b359535157c0087fa260370
Lchasi <1569788669@qq.com>
2024-06-22 16:17:48 +0800

    实现简单的登录注册接口

--- train-member/src/main/java/com/Lchasi/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/Lchasi/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.Lchasi.train.member.controller;
 import com.Lchasi.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,12 @@ public class MemberController {
     @GetMapping("/count")
     public String count() {
         return "" + memberService.count();
+    }
+
+    //注册接口
+    @PostMapping("/register")
+    public long register(String mobile) {
+        return memberService.register(mobile);
     }
 
 }
--- train-member/src/main/java/com/Lchasi/train/member/service/MemberService.java
+++ train-member/src/main/java/com/Lchasi/train/member/service/MemberService.java
@@ -1,15 +1,42 @@
 package com.Lchasi.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.Lchasi.train.member.domain.Member;
+import com.Lchasi.train.member.domain.MemberExample;
 import com.Lchasi.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
     @Resource
     private MemberMapper memberMapper;
 
-    public int count(){
+    public int count() {
         return (int) memberMapper.countByExample(null);//更具条件去查询
+    }
+
+    /**
+     * 手机号注册以及登录功能
+     * @param mobile
+     * @return
+     */
+    public long register(String mobile){
+        MemberExample example = new MemberExample();
+        example.createCriteria().andMobileEqualTo(mobile);//创建条件
+        List<Member> list = memberMapper.selectByExample(example);
+
+        if(CollUtil.isNotEmpty(list)){
+            //已经有人注册
+            throw new RuntimeException("手机号已被注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
