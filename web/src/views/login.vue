0aac9f943d410027e69c3af1de9784b959060e2d
Lchasi <1569788669@qq.com>
2024-06-23 20:01:40 +0800

    增加login路由及页面

---
+++ web/src/views/login.vue
@@ -0,0 +1,65 @@
+<!--负责页面的展现-->
+<template>
+  <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+        label="Username"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+    >
+      <a-input v-model:value="formState.username" />
+    </a-form-item>
+
+    <a-form-item
+        label="Password"
+        name="password"
+        :rules="[{ required: true, message: 'Please input your password!' }]"
+    >
+      <a-input-password v-model:value="formState.password" />
+    </a-form-item>
+
+    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<!--负责逻辑-->
+<script>
+import { defineComponent, reactive } from 'vue';
+export default defineComponent({
+  // 初始化的方法
+  setup() {//绑定的数据，html与js绑定起来
+    const formState = reactive({
+      username: '',
+      password: '',
+      remember: true,
+    });
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
+    return {
+      formState,
+      onFinish,
+      onFinishFailed,
+    };
+  },
+});
+</script>
+<!--负责css-->
+<style>
+
+</style>
