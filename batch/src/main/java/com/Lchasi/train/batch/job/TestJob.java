17def7badeba360d2be75b8af82343fd683644cd
Lchasi <1569788669@qq.com>
2024-07-24 15:31:44 +0800

    集成quartz,和springboot自带的定时任务可以共存

---
+++ batch/src/main/java/com/Lchasi/train/batch/job/TestJob.java
@@ -0,0 +1,14 @@
+package com.Lchasi.train.batch.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("Quartz Job Test");
+    }
+}
